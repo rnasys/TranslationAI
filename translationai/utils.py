920ff069ec8e33bfdb6b5ab1d7d496077b00d769
rnasys <70932317+rnasys@users.noreply.github.com>
2023-07-03 12:48:32 -0400

    Update utils.py

--- translationai/utils.py
+++ translationai/utils.py
@@ -25,8 +25,8 @@ OUT_MAP = np.asarray([[1, 0, 0],
                       [0, 1, 0],
                       [0, 0, 1],
                       [0, 0, 0]])
-# One-hot encoding of the outputs: 0 is for no splice, 1 is for acceptor,
-# 2 is for donor and -1 is for padding.
+# One-hot encoding of the outputs: 0 is for no translation sites, 1 is for TIS,
+# 2 is for TTS and -1 is for padding.
 
 
 def ceil_div(x, y):
@@ -37,11 +37,7 @@ def ceil_div(x, y):
 def create_datapoints(seq, strand, tx_start, tx_end, jn_start, jn_end):
     # This function first converts the sequence into an integer array, where
     # A, C, G, T, N are mapped to 1, 2, 3, 4, 0 respectively. If the strand is
-    # negative, then reverse complementing is done. The splice junctions 
-    # are also converted into an array of integers, where 0, 1, 2, -1 
-    # correspond to no splicing, acceptor, donor and missing information
-    # respectively. It then calls reformat_data and one_hot_encode
-    # and returns X, Y which can be used by Keras models.
+    # negative, then reverse complementing is done. 
 
     #seq = 'N'*(CL_max//2) + seq[CL_max//2:-CL_max//2] + 'N'*(CL_max//2)
     # Context being provided on the RNA and not the DNA
@@ -97,7 +93,7 @@ def reformat_data(X0, Y0):
     # blocks such that the data is broken down into data points where the
     # input is a sequence of length SL+CL_max corresponding to SL nucleotides
     # of interest and CL_max context nucleotides, the output is a sequence of
-    # length SL corresponding to the splicing information of the nucleotides
+    # length SL corresponding to the ORF information of the nucleotides
     # of interest. The CL_max context nucleotides are such that they are
     # CL_max/2 on either side of the SL nucleotides of interest.
 
@@ -150,7 +146,7 @@ def one_hot_encode(Xd, Yd):
 
 def print_topl_statistics(y_true, y_pred):
     # Prints the following information: top-kL statistics for k=0.5,1,2,4,
-    # auprc, thresholds for k=0.5,1,2,4, number of true splice sites.
+    # auprc, thresholds for k=0.5,1,2,4, number of true translation sites.
 
     idx_true = np.nonzero(y_true == 1)[0]
     argsorted_y_pred = np.argsort(y_pred)
